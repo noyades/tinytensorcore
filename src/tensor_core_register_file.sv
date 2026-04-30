0ee25d983ac216fe820b641c45fba1ea47efc5d8
mrdocror <aliaksandr2004@gmail.com>
2026-04-30 19:04:33 -0400

    Unpacked arrays 1

--- src/tensor_core_register_file.sv
+++ src/tensor_core_register_file.sv
@@ -4,19 +4,20 @@ module tensor_core_register_file (
 
     input logic dual_load_enable_in,
     input logic [3:0] dual_load_register_address_in,
-    input logic signed [4:0] dual_load_data_in [2],
+    input logic [9:0] dual_load_data_in_flat,
     
-    output logic signed [4:0] bulk_store_data_out [18]
+    output logic [89:0] bulk_store_data_out_flat
 );
 
     reg signed [4:0] registers [18];
 
-
-    always_comb begin
-        for (int n = 0; n < 18; n++) begin
-            bulk_store_data_out[n] = registers[n];
+    genvar pack_i;
+    generate
+        for (int n = 0; n < 18; n++) begin: pack_output
+            assign bulk_store_data_out_flat[pack_i*5 +: 5] = registers[pack_i];
         end
-    end
+    endgenerate
+    
 
     always_ff @(posedge clock_in) begin
         if (reset_in) begin
